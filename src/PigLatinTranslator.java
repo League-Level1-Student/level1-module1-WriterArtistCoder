74ca7e86625f97be251ae63adba72f74eb196b80
soda <soda@brave-law.lan>
2018-08-27 20:29:54 -0700

    WhackAMole WIP, PigLatinTranslator WIP (Commit at Home)

---
+++ src/PigLatinTranslator.java
@@ -0,0 +1,114 @@
+/** 
+ * Translation icon is by https://www.flaticon.com/authors/freepik
+*/
+
+import java.awt.Dimension;
+import java.awt.event.*;
+import javax.imageio.ImageIO;
+import javax.swing.*;
+
+public class PigLatinTranslator {
+	
+	public JFrame frame = new JFrame();
+	public JPanel panel = new JPanel();
+	
+	public static void main(String[] args) {
+		PigLatinTranslator instance = new PigLatinTranslator();
+		instance.setupGUI();
+	}
+	
+	public void setupGUI() {
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		frame.add(panel);
+		
+		JTextField en = new JTextField(10);
+		JButton go = new JButton();
+		
+		try {
+			ImageIcon i = new ImageIcon(ImageIO.read(new PigLatinTranslator().getClass().getResourceAsStream("translate.svg")));
+		} catch (Exception e1) {
+			System.out.println(e1.toString());
+		}
+		
+		JLabel platin = new JLabel();
+		
+		go.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				platin.setText(translate(en.getText()));
+			}
+		});
+		
+		panel.add(en);
+		panel.add(go);
+		panel.add(platin);
+		
+		frame.setMinimumSize(new Dimension(600, 600));
+	}
+	
+     /**
+     * Method to test whether a character is a letter or not.
+     * @param c The character to test
+     * @return True if it's a letter
+     */
+     private static boolean isLetter(char c) {
+          return ( (c >= 'A' && c <= 'Z') || (c >='a' && c <= 'z') );
+     }
+
+     /**
+     * Method to translate one word into pig latin.
+     * @param word The word in English
+     * @return The pig latin version
+     */
+     private static String pigWord(String word) {
+          int split = firstVowel(word);
+          return word.substring(split)+"-"+word.substring(0, split)+"ay";
+     }
+
+     /**
+     * Method to translate a sentence word by word.
+     * @param s The sentence in English
+     * @return The pig latin version
+     */
+     public String translate(String s) {
+          String latin = "";
+          int i = 0;
+          while (i < s.length()) {
+
+     // Take care of punctuation and spaces
+          while (i < s.length() && !isLetter(s.charAt(i))) {
+               latin = latin + s.charAt(i);
+               i++;
+          }
+
+     // If there aren't any words left, stop.
+          if (i>=s.length()) break;
+
+     // Otherwise we're at the beginning of a word.
+          int begin = i;
+          while (i < s.length() && isLetter(s.charAt(i))) {
+               i++;
+          }
+     // Now we're at the end of a word, so translate it.
+          int end = i;
+          latin = latin + pigWord(s.substring(begin, end));
+          }
+          return latin;
+     }
+
+     /**
+     * Method to find the index of the first vowel in a word.
+     * @param word The word to search
+     * @return The index of the first vowel
+     */
+      private static int firstVowel(String word) {
+          word = word.toLowerCase();
+          for (int i=0; i < word.length(); i++)
+               if (word.charAt(i)=='a' || word.charAt(i)=='e' ||
+                    word.charAt(i)=='i' || word.charAt(i)=='o' ||
+                    word.charAt(i)=='u')
+                    return i;
+               return 0;
+     }
+}
--- src/WhackAMole.java
+++ src/WhackAMole.java
@@ -14,56 +14,55 @@ public class WhackAMole implements ActionListener {
 
 	public JFrame frame = new JFrame();
 	public JPanel panel = new JPanel();
-	
+
 	public static void main(String[] args) {
 		WhackAMole instance = new WhackAMole();
 		instance.setupGUI();
-		
+
+		int i = 0;
 		while (true) {
-			instance.panel.removeAll();
-			instance.drawButtons();
-			
-			try {
-				Thread.sleep(1000);
-			} catch (Exception x) {
-				
+			if (i > 100) {
+				instance.panel.removeAll();
+				instance.drawButtons();
+				i = 0;
 			}
+			i++;
 		}
 	}
-	
+
 	public void setupGUI() {
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setMinimumSize(new Dimension(600, 600));
-		
+
 		frame.add(panel);
 	}
-	
+
 	void speak(String words) {
-	     try {
-	          Runtime.getRuntime().exec("say " + words).waitFor();
-	     } catch (Exception e) {
-	          e.printStackTrace();
-	     }
+		try {
+			Runtime.getRuntime().exec("say " + words).waitFor();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
-	
+
 	private void playSound(String fileName) {
-	     AudioClip sound = JApplet.newAudioClip(getClass().getResource(fileName));
-	     sound.play();
+		AudioClip sound = JApplet.newAudioClip(getClass().getResource(fileName));
+		sound.play();
 	}
-	
+
 	public void drawButtons() {
 		int moleNum = new Random().nextInt(24);
 		for (int i = 0; i < 24; i++) {
 			JButton b = new JButton("");
-			
+
 			if (i == moleNum) {
 				b.setText("MOLE!");
 				b.addActionListener(this);
 			} else {
 				b.addActionListener(this);
 			}
-			
+
 			panel.add(b);
 		}
 	}
@@ -71,16 +70,16 @@ public class WhackAMole implements ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		JButton button = (JButton) e.getSource();
-	
+
 		if (button.getText().equals("MOLE!")) {
 			playSound("pong copy.wav");
 			try {
 				Thread.sleep(1000);
 			} catch (Exception x) {
-				
+
 			}
 		} else {
-			String[] missed = {"You can't catch me", "I'm over there", "Missed me"};
+			String[] missed = { "You can't catch me", "I'm over there", "Missed me" };
 			speak(missed[new Random().nextInt(missed.length)]);
 		}
 	}
